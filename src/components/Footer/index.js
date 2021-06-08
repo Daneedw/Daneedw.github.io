a3cf6aaa6f3314e91e65bfcbcf2e2bbfe521a723
Dane Edwards <Daneedw@gmail.com>
2021-06-08 10:51:27 -0500

    header and footer added. next working on contact and projects

---
+++ src/components/Footer/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import styles from "./style.module.css"
+export default function Footer() {
+    return (
+        <footer className={styles.footer}>
+            Copyright © 2021 Dane Edwards
+        </footer>
+    )
+}
---
+++ src/components/Navbar/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import styles from "./style.module.css"
+import {
+    Link
+  } from "react-router-dom";
+export default function Navbar() {
+
+  return (
+    <nav>
+      <ul className={styles.navParent}>
+        {/* <li > <Link to="/">Home </Link></li> */}
+        <li > <Link to="/projects">Projects </Link></li>
+        <li > <Link to="/contact">Contact </Link></li>
+      </ul>
+    </nav>
+  );
+}
